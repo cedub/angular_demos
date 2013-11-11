19dfb4c8cdc7ffd9b0686a4452c513ec2e26cb2c
Christian West <christian@meosphere.com>
2013-11-11 10:21:50 -0700

    Another revision to the proposed Angular scaffold

--- apps/core/static/core/js/app/app.js
+++ apps/core/static/core/js/app/app.js
@@ -1,21 +1,16 @@
-angular.module('firstApp', ['ngRoute', 'controllers'])
+'use strict';
 
-    .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider
-            .when('/my-app',
-                {
-                    controller: 'TestingController',
-                    templateUrl: GLOBAL.STATIC_URL + 'core/js/app/partials/temp-partial.html'
-                });
-    }]);
 
-angular.module('navBar', ['ngRoute'])
-    .controller('NavbarController', function ($scope, $location) {
-        $scope.getClass = function (path) {
-            if ($location.path().substr(0, path.length) == path) {
-                return true
-            } else {
-                return false;
-            }
-        }
-    });
+// Declare app level module which depends on filters, and services
+angular.module('lkApp', [
+  'ngRoute',
+  'lkApp.filters',
+  'lkApp.services',
+  'lkApp.directives',
+  'lkApp.controllers'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {templateUrl: GLOBAL.STATIC_URL + 'core/js/app/partials/partial1.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/view2', {templateUrl: GLOBAL.STATIC_URL + 'core/js/app/partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
