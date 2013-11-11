967d3e6a0946853694427045bf5a707a9e522dba
Christian West <christian@meosphere.com>
2013-11-11 11:05:25 -0700

    Scaffold based off of angular-seed (the last before switching over to ng-boilerplate)

--- apps/core/static/core/js/app/directives.js
+++ apps/core/static/core/js/app/directives.js
@@ -3,9 +3,15 @@
 /* Directives */
 
 
-angular.module('lkApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+angular.module('lkApp.directives', [])
+    .directive('appVersion', ['version', function(version) {
+        return function(scope, elm, attrs) {
+            elm.text(version);
+        };
+    }])
+    .directive('lknav', function () {
+        return {
+            restrict: 'E',
+            templateUrl: GLOBAL.STATIC_URL + 'core/js/app/partials/nav.html'
+        };
+    });
