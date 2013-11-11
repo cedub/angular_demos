01705d36d541ffdb4ffac4bcd87663749258d298
Christian West <christian@meosphere.com>
2013-11-11 06:27:38 -0700

    Removed unnecessary route file

--- apps/core/static/core/js/app/route.js
+++
@@ -1,9 +0,0 @@
-
-app.config(function ($routeProvider) {
-    $routeProvider
-        .when('/my-app',
-            {
-                controller: 'TestingController',
-                templateUrl: '/app/partials/temp-partial.html'
-            });
-});
