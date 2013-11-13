e582edb7d1d59f28963bf75e4c654ed9aa5ec26f
Christian West <christian@meosphere.com>
2013-11-13 08:33:00 -0700

    Changed scaffold to be JS only --- no need to include django

--- apps/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from .views import Index
-
-
-urlpatterns = patterns('',
-    url(r'^$', Index.as_view(), name='index'),
-)
--- apps/core/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.views.generic.base import View
-from django.conf import settings
-
-
-class Index(View):
-    def get(self, request):
-        return render(request, 'core/index.html')
